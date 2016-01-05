2f0b847fa9b0a24b56adb0c9a800243b92e794e6
hanulpark91 <hanul.park@hanulpark-ltm2.internal.salesforce.com>
2016-01-05 14:45:27 -0800

    updated home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  get 'home/index'
 
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
